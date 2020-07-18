855d373d7c9602432a21c7426866dd48a4153925
Shika <greffinroid@ymail.com>
2020-07-18 17:42:39 +0700

    Added script to fix lint
    From nuxtjs.org/guide/development-tools/

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'vue'],
   // add your custom rules here
   rules: {},
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -78,5 +78,20 @@ export default {
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
    */
-  build: {},
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+        })
+      }
+    },
+  },
 }
