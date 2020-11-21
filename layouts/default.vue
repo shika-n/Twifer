64668d3a345875679e55904e735122a88e9afb6e
Shika <greffinroid@ymail.com>
2020-11-21 12:57:31 +0700

    Added splash screen

--- layouts/default.vue
+++ layouts/default.vue
@@ -106,3 +106,30 @@ export default {
   },
 }
 </script>
+
+<!--
+<v-dialog v-model="notAuth" persistent width="400">
+  <v-card>
+    <v-card-title>
+      Login with Twitter
+    </v-card-title>
+    <v-card-text>
+      Allow this app to access your account to track favorites, following,
+      and followers.
+    </v-card-text>
+    <v-card-actions>
+      <v-container>
+        <v-row justify="end">
+          <v-btn
+            :color="`light-blue lighten-2`"
+            href="api/twitter/auth"
+            dark
+          >
+            Authenticate
+          </v-btn>
+        </v-row>
+      </v-container>
+    </v-card-actions>
+  </v-card>
+</v-dialog>
+-->
